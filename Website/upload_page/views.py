88bf15df1d64fcb7bbe28a8b9b083e6c3bc49f5d
023proz <023zhangjiaoshou@gmail.com>
2020-07-27 15:13:07 -0700

    change file path

--- Website/upload_page/views.py
+++ Website/upload_page/views.py
@@ -3,7 +3,8 @@ from .models import Document
 from .forms import DocumentForm
 import os
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
+from django.conf import settings
+from django.http import HttpResponse, Http404
 from pathlib import Path
 
 @csrf_exempt
@@ -20,10 +21,20 @@ def upload(request):
 
  
 def handle_uploaded_file(file, filename):
-    path = '/home/Download/'
+    path = '/home/ubuntu/'
     if not os.path.exists(path):
         os.mkdir(path)
  
     with open(path + filename, 'wb+') as destination:
         for chunk in file.chunks():
             destination.write(chunk)
+
+@csrf_exempt
+def download(request, path):
+    file_path = os.path.join(settings.MEDIA_ROOT, path)
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            return response
+    raise Http404
